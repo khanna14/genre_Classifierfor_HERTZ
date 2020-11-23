a1dcb1f7c8ea111bf4f0e3c385e73b25b30171fc
Rahul Khanna <55352426+khanna14@users.noreply.github.com>
2020-11-23 19:22:13 +0530

    Update settings.py

--- django-jquery-file-upload/settings.py
+++ django-jquery-file-upload/settings.py
@@ -88,6 +88,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
